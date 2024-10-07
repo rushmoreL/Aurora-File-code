67b9efe7bd7fd3a5f562110461b5556c04b52a71
rushmoreL <148906805+rushmoreL@users.noreply.github.com>
2024-10-07 17:48:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function generateFileName() {
+function generateFileNames() {
     const code = document.getElementById('codeInput').value;
     const option = document.getElementById('option').value;
     const season = document.getElementById('seasonInput').value;
@@ -6,7 +6,8 @@ function generateFileName() {
     const lines = code.split('\n');
     
     if (lines.length < 5 || !season) {
-        document.getElementById('result').innerText = "Invalid input.";
+        document.getElementById('auroraName').innerText = "Invalid input.";
+        document.getElementById('schmName').innerText = "";
         return;
     }
 
@@ -17,15 +18,19 @@ function generateFileName() {
     const moidNumber = moid.split(' ')[1];
     const productNumber = productCode.split('-')[0];
 
-    const newFileName = `${moidNumber}_${option}_${description.replace(/ /g, '_')}_${season}_${productNumber}`;
-    
-    document.getElementById('result').innerText = newFileName;
+    // Aurora Upload Name
+    const auroraFileName = `${moidNumber}_${option}_${description.replace(/ /g, '_')}_${season}_${productNumber}`;
+    document.getElementById('auroraName').innerText = `Aurora Upload Name: ${auroraFileName}`;
+
+    // Schm Name
+    const schmFileName = `${productNumber}_${description.replace(/ /g, '_')}_schm_${season}_T${option}`;
+    document.getElementById('schmName').innerText = `Schm Name: ${schmFileName}`;
 }
 
-function copyToClipboard() {
-    const result = document.getElementById('result').innerText;
-    if (result && result !== "Invalid input.") {
-        navigator.clipboard.writeText(result).then(() => {
+function copyToClipboard(elementId) {
+    const textToCopy = document.getElementById(elementId).innerText.split(': ')[1];
+    if (textToCopy) {
+        navigator.clipboard.writeText(textToCopy).then(() => {
             alert('File name copied to clipboard!');
         }).catch(err => {
             console.error('Could not copy text: ', err);
